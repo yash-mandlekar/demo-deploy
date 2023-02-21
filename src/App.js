06c1dbeef0360de96246b12d7f33c7f3ae4b739c
yash-mandlekar <ymandlekar9826@gmail.com>
2023-02-21 12:41:25 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React, { useEffect, useState } from "react";
 const App = () => {
   const [backend, setBackend] = useState(null);
   const fetchbackend = async () => {
-      const test = await fetch(`${process.env.REACT_APP_API_URL}/test`);
-      console.log(test);
+    const test = await fetch(`${process.env.REACT_APP_API_URL}/test`);
     const json = await test.json();
     setBackend(json);
   };
