182f0484dc30392600972b44fe51b51b4a500622
yash-mandlekar <ymandlekar9826@gmail.com>
2023-02-23 20:25:42 +0530

    updated .env

--- src/Components/Axios/Axios.jsx
+++ src/Components/Axios/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
